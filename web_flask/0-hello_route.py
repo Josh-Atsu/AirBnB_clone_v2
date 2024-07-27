44cdffe7109f4bc6e61b154de46b62f4f40dc72c
Joshua <atsujoshua67@gmail.com>
2024-07-26 23:21:54 -0700

    Hello hbnb

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+web application is listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """Display <Hello HBNB!>"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+web application is listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def Hello():
+    """Display <Hello HBNB!>"""
+    return "Hello HBNB!"
+@app.route("/hbnb")
+def hbnb():
+    """display HBNH"""
+    return "HBNH"
+
+
+if __name__ == "__main__":
+    app.run()
